dd74d648a4bcb98a55bf62dd7238a8ec584a8ab0
davpal <paluchdaw@gmail.com>
2020-08-21 15:51:40 +0200

    Redesign category chooser

--- src/quiz/CategoryChooser.js
+++ src/quiz/CategoryChooser.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import categories from '../categories/categories';
 
 const CategoryChooser = () => {
   const history = useHistory();
+  const [category, setCategory] = useState(categories[0].code);
 
   function startQuiz(category) {
     history.push(`/quiz/${category}`);
@@ -14,11 +15,23 @@ const CategoryChooser = () => {
   );
 
   return <>
-  <h2>Choose category</h2>
-  <select name="trivia_category" className="category-chooser"
-    onChange={(e) => startQuiz(e.target.value)}>
-    {options}
-  </select>
+    <div className="bg-text">Quiz</div>
+    <div className="header">
+      <h1 className="app-title">quizapp</h1>
+      <span className="app-subtitle">in react</span>
+    </div>
+    <div className="form-group">
+      <label for="category">Category</label>
+      <select name="category" className="category-chooser"
+        onChange={(e) => setCategory(e.target.value)}>
+        {options}
+      </select>
+    </div>
+    <div className="form-group">
+      <label for="question_count">Number of questions</label>
+      <input name="question_count" type="text"></input>
+    </div>
+    <button className="new-quiz-button" onClick={() => startQuiz(category)}>Start quiz</button>
   </>
 };
 
